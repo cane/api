43d46acb4aa131dc92f69387e72962e3444db431
Sun Yat-ton <imsunyitao@gmail.com>
2021-03-20 13:37:41 +0800

    Add MessageFormat support Exception msg.

--- src/main/java/org/canedata/exception/DataAccessException.java
+++ src/main/java/org/canedata/exception/DataAccessException.java
@@ -1,8 +1,11 @@
 package org.canedata.exception;
 
+import java.text.MessageFormat;
+
 public class DataAccessException extends RuntimeException {
 
 	private static final long serialVersionUID = -1482548993264902769L;
+	private int status = 0;
 
 	public DataAccessException() {
 		super();
@@ -20,4 +23,45 @@ public class DataAccessException extends RuntimeException {
 		super(cause);
 	}
 
+	public static DataAccessException of(int status) {
+		return new DataAccessException().status(status);
+	}
+
+	public static DataAccessException of(String message) {
+		return new DataAccessException(message);
+	}
+
+	public static DataAccessException of(String message, Object...args) {
+		if(args == null || args.length == 0) {
+			return new DataAccessException(message);
+		}
+
+		MessageFormat mf = new MessageFormat(message);
+		return new DataAccessException(mf.format(args));
+	}
+
+	public static DataAccessException of(int status, String message) {
+		return new DataAccessException(message).status(status);
+	}
+
+	public static DataAccessException of(int status, String message, Object...args) {
+		return DataAccessException.of(message, args).status(status);
+	}
+
+	public static DataAccessException of(int status, Throwable throwable) {
+		return new DataAccessException(throwable).status(status);
+	}
+
+	public DataAccessException status(int status) {
+		this.status = status;
+		return this;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
 }
